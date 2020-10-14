3771f90ae97e848f7e1cc357bc5ea6a1a4b2541e
Onesinus Saut Parulian <onesinus231@gmail.com>
2020-10-14 18:54:28 +0700

    Update queue.py

--- queue.py
+++ queue.py
@@ -13,7 +13,7 @@ def tampilkanDaftarAntri(data):
         print(d)
 
 while True:
-    perintah = input('Tekan 1 untuk antrian CS, tekan 2 untuk antrian teller, tekan NOW untuk check antrian sekarang: \n')
+    perintah = input(' Tekan 1 untuk antrian CS, \n tekan 2 untuk antrian teller, \n tekan NOW untuk check antrian sekarang: \n')
     if perintah == '1':
         no_urut_cs += 1
         antrian_cs.append(no_urut_cs)
@@ -23,14 +23,18 @@ while True:
         antrian_teller.append(no_urut_teller)
         print('Antrian ke: ' , no_urut_teller)
     elif perintah == 'OUT CS':
-        print(antrian_cs.popleft())
+        print('No antrian yang keluar: ' , antrian_cs.popleft())
     elif perintah == 'OUT TELLER':
-        print(antrian_teller.popleft())
+        print('No antrian yang keluar: ', antrian_teller.popleft())
+    elif perintah == 'NOW CS':
+        print ('No antrian terakhir: ', no_urut_cs)
     elif perintah == 'NOW TELLER':
-        print (antrian_teller)
-    elif perintah == 'SHOW QUEUE CS':
+        print ('No antrian terakhir: ', no_urut_teller)
+    elif perintah == 'SHOW CS':
+        print('Daftar antrian yang ada pada CS \n')
         tampilkanDaftarAntri(antrian_cs)
-    elif perintah == 'SHOW QUEUE TELLER':
+    elif perintah == 'SHOW TELLER':
+        print('Daftar antrian yang ada pada Teller \n')
         tampilkanDaftarAntri(antrian_teller)
     elif perintah == 'EXIT':
         break;
